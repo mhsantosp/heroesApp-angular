2808ab51207f0433c4364e251a8df6fc1fed00d4
Maria Helena SP <msantospineda1@gmail.com>
2021-07-13 16:13:03 -0500

    Creación ruta principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 
 
+const routes: Routes = [
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+]
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
